5167c8271450ed7c34e9395737297e9ed0786595
Emerson Macedo <emerson@corp.globo.com>
2010-10-23 11:45:39 -0200

    reste no reroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubyconf::Application.config.secret_token = 'd0da5e6c28005348b11eae9cd86fe42650ef54d6efbde3034508e37aa0df14c3d8fe0c1b506c75dea4fd7ca22927c4a681c5da0b09fc701b5076c623cbabf540'
