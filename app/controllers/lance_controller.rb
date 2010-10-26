790df873d15fe70c4d8701dc2951b5f9e08fdb17
Emerson Macedo <emerson@corp.globo.com>
2010-10-25 22:43:53 -0200

    Agora recebee o time também

--- app/controllers/lance_controller.rb
+++ app/controllers/lance_controller.rb
@@ -2,7 +2,7 @@ class LanceController < ApplicationController
   def enviar
     HTTParty.post("http://#{AppConfig['node_host']}/lance?token=#{AppConfig['node_token']}", 
       :headers => {"Content-Type" => "application/json"},
-      :body => {:texto => params[:texto], :link => params[:link]}.to_json
+      :body => {:time => params[:time], :texto => params[:texto]}.to_json
     )
     flash[:notice] = "lance enviado com sucesso"
     redirect_to :novo
--- spec/controllers/lance_controller_spec.rb
+++ spec/controllers/lance_controller_spec.rb
@@ -2,12 +2,12 @@ require 'spec_helper'
 
 describe LanceController do
   it 'should post to emerleite.no.de' do
-    lance = {:texto => 'Gol do Flamengo', :link => 'http://www.youtube.com/watch?v=zr3N1hm7W7M'}
+    lance = {:time => 'flamengo', :texto => 'Gol do Flamengo'}
     
     HTTParty.should_receive(:post).with("http://#{AppConfig['node_host']}/lance?token=#{AppConfig['node_token']}",
       :headers => {"Content-Type" => "application/json"},
       :body => lance.to_json)
-    post :enviar, :texto => 'Gol do Flamengo', :link => 'http://www.youtube.com/watch?v=zr3N1hm7W7M'
+    post :enviar, :time => 'flamengo', :texto => 'Gol do Flamengo'
     
     flash[:notice].should eq("lance enviado com sucesso")
     response.should redirect_to :action => "novo"
